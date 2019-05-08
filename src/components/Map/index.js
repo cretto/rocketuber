4556e4a7d71d209f61e40e411dc2021407c2cbc5
cretto <creto_boy@hotmail.com>
2019-05-07 21:27:35 -0300

    rocketuber - fix code

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -65,7 +65,7 @@ export default class Map extends Component {
   };
 
   handleBack = () => {
-    this.setState({ destination: null});
+    this.setState({ destination: null });
   };
 
   render() {
